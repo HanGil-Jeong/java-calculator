78c1f63d691fd2aef3058f155697d742b8474b38
HanGil <jhg9240@gmail.com>
2025-09-30 20:26:50 +0900

    refactor: if-else -> switch-case 변경하여 가독성 높임

--- src/com/example/calculator/lv1/Calculator.java
+++ src/com/example/calculator/lv1/Calculator.java
@@ -5,53 +5,51 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        int number1;
-        int number2;
-        String operation;
+        int firstNumber;
+        int secondNumber;
+        int result;
+        String operator;
         String exit = "";
 
+        Scanner sc = new Scanner(System.in);
+
         while (!exit.equals("exit")) {
-            System.out.print("숫자를 입력해주세요: ");
-            number1 = scanner.nextInt();
-            System.out.println(number1);
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            firstNumber = sc.nextInt();
 
-            System.out.println("-----------");
-            System.out.println("+, -, x, %");
-            System.out.println("-----------");
-            System.out.print("사칙연산 선택: ");
-            operation = scanner.next();
+            System.out.print("사칙연산(+, -, *, /)을 입력하세요: ");
+            operator = sc.next();
 
-            System.out.print("숫자를 입력해주세요: ");
-            number2 = scanner.nextInt();
-            System.out.println(number1 + operation + number2);
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            secondNumber = sc.nextInt();
 
-            if (operation.equals("+")) {
-                int result = number1 + number2;
-                System.out.println(number1 + operation + number2 + "=" + result);
+            switch (operator) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    System.out.println(firstNumber + " " + operator + " " + secondNumber + " = " + result);
+                    break;
 
-            } else if (operation.equals("-")) {
-                int result = number1 - number2;
-                System.out.println(number1 + operation + number2 + "=" + result);
+                case "-":
+                    result = firstNumber - secondNumber;
+                    System.out.println(firstNumber + " " + operator + " " + secondNumber + " = " + result);
+                    break;
 
-            } else if (operation.equals("x") || operation.equals("*")) {
-                int result = number1 * number2;
-                System.out.println(number1 + operation + number2 + "=" + result);
+                case "*":
+                    result = firstNumber * secondNumber;
+                    System.out.println(firstNumber + " " + operator + " " + secondNumber + " = " + result);
+                    break;
 
-            } else if (operation.equals("%") || operation.equals("/")) {
-                if (number2 != 0) {
-                    int result = number1 / number2;
-                    System.out.println(number1 + operation + number2 + "=" + result);
-
-                } else {
-                    System.out.println("0으로 나눌 수 없습니다.");
-                }
-            } else {
-                System.out.println("잘못 누르셨습니다.");
+                case "/":
+                    if (secondNumber != 0) {
+                        result = firstNumber / secondNumber;
+                        System.out.println(firstNumber + " " + operator + " " + secondNumber + " = " + result);
+                        break;
+                    } else {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                    }
             }
-
             System.out.print("더 계산하시겠습니까? (exit입력 시 종료)");
-            exit = scanner.next();
+            exit = sc.next();
 
         }
     }
